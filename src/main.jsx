145e6fc58970f74debf935bdd4909aad5722de64
Babacar Ciss <kratosnvidia@gmail.com>
2022-12-28 00:22:50 +0000

    fixing deployment issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import GlobalStyle from "./styles/globalStyle";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    font-family: "Inter", sans-serif;
+  }
+  
+  body {
+    font-family: "Inter", sans-serif;
+  }
+
+ 
+`;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
